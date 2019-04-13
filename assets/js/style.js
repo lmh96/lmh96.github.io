dfc33353451252cd2c59017843fa5d4ca47d6682
lmh96 <loganhill23@gmail.com>
2019-04-13 01:51:39 -0400

    changed to be portfolio webpage prototype

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,7 +1,29 @@
-$(".nav-btn").on("click", function() {
-    window.location=$(this).attr("href");
+$(document).on("click", "#nav-show", function() {
+    let nav = $("nav");
+    nav.css("height", "50px");
+    $(this).removeAttr("id");
+    $(this).attr("id", "nav-hide");
 });
 
-$(".project-tile").on("click", function() {
+$(document).on("click", "#nav-hide", function() {
+    let nav = $("nav");
+    nav.css("height", "0px");
+    $(this).removeAttr("id");
+    $(this).attr("id", "nav-show");
+});
+
+$(".fa-github").on("click", function() {
+    window.open("https://github.com/lmh96", "_blank");
+})
+
+$(".fa-linkedin-in").on("click", function() {
+    window.open("https://www.linkedin.com/in/logan-hill-86828b166", "_blank");
+})
+
+$(".fa-code").on("click", function() {
+    window.open($(this).attr("href"), "_blank");
+});
+
+$(".project-div").on("click", function(e) {
     window.open($(this).attr("href"), "_blank");
 });
